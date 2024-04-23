a6ab3be9d96eaf07389bad55f3364c31ce1fb961
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-23 17:10:18 +0330

    coding api for login

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -1,4 +1,31 @@
-const handler = (req, res) => {
-    // codes
+import connectToDB from "@/configs/db";
+import UserModel from "@/models/User";
+const handler = async (req, res) => {
+    if (req.method !== 'POST') {
+        return false;
+    }
+
+    try {
+        connectToDB();
+
+        const { identifirer, password } = req.body;
+
+        if (!identifirer.trim() || !password.trim()) {
+            return res.status(422).json({ message: "Data is not valid !!" })
+        }
+
+        const user = await UserModel.findOne({
+            $or: [{ username: identifirer }, { email: identifirer }]
+        })
+
+        if(!user) {
+            return res.status(404).json({message:"User not found"})
+        }
+
+        
+
+    } catch (error) {
+        // Codes
+    }
 }
 export default handler;
