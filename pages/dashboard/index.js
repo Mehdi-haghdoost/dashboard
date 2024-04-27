0802f01fce76b8c1e6e20d00cdef2343e9c8cb45
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-27 11:44:30 +0330

    show user info in dashboard with server component

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,19 +1,22 @@
+import connectToDB from "@/configs/db";
+import UserModel from '@/models/User'
 import { verifyToken } from "@/utils/auth";
 import { redirect } from "next/dist/server/api-utils";
 import React from "react";
 
-function Dashboard() {
+function Dashboard({user}) {
   return (
     <>
-      <h1>Amin - Saeedi - Welcome To Dashboard</h1>
+      <h1>{user.firstname} - {user.lastname} - Welcome To Dashboard</h1>
     </>
   );
 }
 
 export async function getServerSideProps(context) {
+  connectToDB();
 
   const { token } = context.req.cookies;
- 
+
   if (!token) {
     return {
       redirect: {
@@ -23,6 +26,7 @@ export async function getServerSideProps(context) {
   }
 
   const tokenPayload = verifyToken(token);
+  console.log(tokenPayload);
 
   if (!tokenPayload) {
     return {
@@ -32,8 +36,17 @@ export async function getServerSideProps(context) {
     }
   }
 
+  const user = await UserModel.findOne(
+    {
+      email: tokenPayload.email,
+    },
+    "_id firstname lastname"
+  )
+
   return {
-    props: {},
+    props: {
+      user : JSON.parse(JSON.stringify(user))
+    },
   }
 }
 
