1e1ce0f73a24613fa5e890157b5ed0b7449cc5b2
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-27 08:41:04 +0330

    compare password

--- utils/auth.js
+++ utils/auth.js
@@ -1,20 +1,25 @@
 import { hash } from "bcrypt";
 import { sign } from "jsonwebtoken";
-
+import { compare } from "bcrypt";
 const hashPasword = async (password) => {
 
-    const hashedPassword = await hash(password, 12);
+  const hashedPassword = await hash(password, 12);
 
-    return hashedPassword;
+  return hashedPassword;
 }
 
 const generateToken = (data) => {
-    const token = sign({ ...data }, process.env.privateKey, {
-      // algorithm: ''
-      expiresIn: "24h",
-    });
-  
-    return token;
-  };
+  const token = sign({ ...data }, process.env.privateKey, {
+    // algorithm: ''
+    expiresIn: "24h",
+  });
 
-export { hashPasword, generateToken };
+  return token;
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+}
+
+export { hashPasword, generateToken, verifyPassword };
