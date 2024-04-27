0532cecae05f1049dc6953464c5fcebed6f1c8b0
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-27 10:38:12 +0330

    get token in dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,3 +1,5 @@
+import { verifyToken } from "@/utils/auth";
+import { redirect } from "next/dist/server/api-utils";
 import React from "react";
 
 function Dashboard() {
@@ -6,6 +8,33 @@ function Dashboard() {
       <h1>Amin - Saeedi - Welcome To Dashboard</h1>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+
+  const { token } = context.req.cookies;
+ 
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/signin',
+      },
+    }
+  }
+
+  const tokenPayload = verifyToken(token);
+
+  if (!tokenPayload) {
+    return {
+      redirect: {
+        destination: '/signin',
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
 
 export default Dashboard;
