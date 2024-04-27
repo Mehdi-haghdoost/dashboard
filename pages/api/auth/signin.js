a1a79f074894004a8db26df025209e372086565c
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-27 08:41:37 +0330

    verify password for login

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -1,5 +1,6 @@
 import connectToDB from "@/configs/db";
 import UserModel from "@/models/User";
+import { generateToken, verifyPassword } from "@/utils/auth";
 const handler = async (req, res) => {
     if (req.method !== 'POST') {
         return false;
@@ -18,14 +19,31 @@ const handler = async (req, res) => {
             $or: [{ username: identifirer }, { email: identifirer }]
         })
 
-        if(!user) {
-            return res.status(404).json({message:"User not found"})
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
         }
 
-        
+        const isValidPassword = await verifyPassword(password, user.password)
+
+        if (!isValidPassword) {
+            return res.status(422).json({ message: "Username or Password is not correct" })
+        }
+
+        const token = generateToken({ email: user.email })
+
+
+        return res
+            .setHeader('Set-Cookie', serialize('token', token, {
+                httpOnly: true,
+                path: '/',
+                maxAge: 60 * 60 * 24
+            }))
+            .status(200)
+            .json({ message: "User Logged in successfully :))" })
 
     } catch (error) {
-        // Codes
+        return res.status(500)
+            .json({ message: "Unknow Internal Server Error !!", error: err })
     }
 }
 export default handler;
